ee929671e964cb79fe19991f9d42fb42c1600f3c
Mano Vikram <manovik18@gmail.com>
2025-03-19 02:10:25 +0530

    Added Shadcn UI Dialog component; Added a pop up dialog which appears only when the user is not logged in

--- components/shared/LeftSidebar.jsx
+++ components/shared/LeftSidebar.jsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Button } from '../ui/button'
+import { DialogClose } from '@radix-ui/react-dialog'
 
 function LeftSidebar() {
   return (
@@ -37,12 +40,40 @@ function LeftSidebar() {
           </Link>
         </div>
 
-        <div className="hover:bg-dark-3 px-4.5 py-3 rounded-lg">
-          <Link href='/likes' className='flex items-center'>
-            <Image src='/user.svg' alt='likes icon' width={28} height={28} />
-          </Link>
-        </div>
+        {/* TODO: Make it a component */}
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className="hover:bg-dark-3 px-4.5 py-3 rounded-lg cursor-pointer">
+              <Image src='/user.svg' alt='likes icon' width={28} height={28} />
+            </div>
+          </DialogTrigger>
+
+          <DialogContent className="sm:max-w-[520px] px-14 py-16 flex flex-col justify-center items-center bg-dark-4 border-none rounded-3xl [&>button]:hidden">
+            <DialogHeader>
+              <DialogTitle asChild>
+                <div className="text-heading1-bold text-white text-center">
+                  Say more with Threads
+                </div>
+              </DialogTitle>
+
+              <DialogDescription className="text-center">
+                Join Threads to share thoughts, find out what's going on, follow your people and more.
+              </DialogDescription>
+            </DialogHeader>
+
+            <DialogFooter>
+              <Button className="mt-8 p-5 h-full gap-14 bg-dark-4 border-3 border-dark-3 rounded-2xl cursor-pointer">
+                <Image src="/google-logo.svg" alt='google logo' width={48} height={48} />
+
+                <p className='text-base1-semibold'>Continue with Google</p>
+
+                <Image src="/right-arrow.svg" alt='right arrow icon' width={16} height={16} />
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
+
 
       <div className="flex flex-col gap-3">
         <div className="hover:bg-dark-3 px-4.5 py-3 rounded-lg">
