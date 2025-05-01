a8f17b1d01c79bc1b7a21df3c35597e820bd2f72
Mano Vikram <manovik18@gmail.com>
2025-05-01 17:17:09 +0530

    Added feature to search with UI and functionality

--- app/(root)/search/page.jsx
+++ app/(root)/search/page.jsx
@@ -1,10 +1,16 @@
 import SearchComponent from '@/components/shared/SearchComponent'
+import { auth } from '@/lib/auth'
+import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 
-function SearchPage() {
+async function SearchPage() {
+    const session = await auth()
+
     return (
         <main className="flex flex-col h-screen items-center justify-start bg-dark-3 rounded-t-3xl p-6">
-            <SearchComponent />
+            <SessionProvider session={session}>
+                <SearchComponent />
+            </SessionProvider>
         </main>
     )
 }
--- components/shared/SearchComponent.jsx
+++ components/shared/SearchComponent.jsx
@@ -5,6 +5,7 @@ import { Loader2 } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
+import ThreadCard from '../cards/ThreadCard'
 
 function SearchComponent() {
     const [isLoading, setIsLoading] = useState(false)
@@ -28,19 +29,27 @@ function SearchComponent() {
     return (
         <>
             <div className="flex w-full bg-dark-2 rounded-4xl px-5 py-2">
-                <Image src="/search.svg" width={16} height={16} />
+                <Image src="/search.svg" alt='search-icon' width={16} height={16} />
 
                 <Input type="text" id="search" placeholder="Search" className="w-full text-white px-3 py-4 selection:bg-blue-400 selection:text-black border-none outline-none focus-visible:ring-0" onChange={(event) => setSearchQuery(event.target.value)} />
             </div>
 
             {isLoading && <Loader2 className="animate-spin text-white mt-20" />}
 
-            {searchResults?.posts?.map((post) => (
-                <p>{post.content}</p>
+            {searchResults?.posts?.slice(0, 5)?.map((post) => (
+                <ThreadCard key={post.id} postID={post.id} username={post.user.id} userImage={post.user.image} content={post.content} createdAt={post.created_at} />
             ))}
 
-            {searchResults?.users?.map((user) => (
-                <p>{user.name}</p>
+            {searchResults?.users?.slice(0, 5)?.map((user) => (
+                <div key={user.id} className="flex flex-row w-full gap-3 px-6 pt-6 py-2 cursor-pointer">
+                    <Image src={user.image} alt='user-image' height={36} width={36} className='h-9 w-9 rounded-full object-fill' />
+
+                    <div className="flex flex-col justify-start items-start gap-1.5">
+                        <p className='font-medium text-white leading-3 m-0'>{user.name}</p>
+
+                        <p className='text-gray-2'>{user.username}</p>
+                    </div>
+                </div>
             ))}
         </>
     )
